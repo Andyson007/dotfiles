75ddb38d2fe942316afc9fa6ef556cf96ac69742
Andyson007 <andreas.jan.van.der.meulen@gmail.com>
2025-06-03 22:44:21 +0200

    fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,11 @@
 if status is-interactive
+    zoxide init fish | source
+    atuin init fish --disable-up-arrow | source
+
+
     alias 'a'='eza -AMl --git'
     alias 'l'='eza -Ml --git'
     alias 'weather'='curl wttr.in'
     alias 'lg'=lazygit
+    fish_vi_key_bindings
 end
